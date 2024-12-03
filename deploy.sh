55cdb7234ce078999df6bd46d9f137e07ae0aab0
Luis Hernandez <soyluishs@gmail.com>
2024-12-03 15:06:39 -0600

    ravis.yml para CD 18

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # deploy.sh
 # Cambia FTP_ADDRESS con tu servidor y ruta correctos
-find . -type f -exec curl -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://195.179.238.210/domains/luishs.com/public_html/travis2/{} \;
+find . -type f -exec curl -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://195.179.238.210/travis2/{} \;
